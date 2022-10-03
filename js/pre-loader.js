b6cc1359110edcb5af84ffa67a18f38df513997d
3s Studio <linardsliepenieks@gmail.com>
2022-10-03 15:18:55 +0300

    pre-loader js

--- js/pre-loader.js
+++ js/pre-loader.js
@@ -2,14 +2,14 @@ const preloader = document.getElementById('pre-loader');
 
 
 const fadeOutEffect = setInterval(() => {
-  if (!preloader.style.opacity) {
-    preloader.style.opacity = 1;
-  }
-  if (preloader.style.opacity > 0) {
-    preloader.style.opacity -= 0.1;
-  } else {
-    clearInterval(fadeEffect);
-  }
-}, 100);
+    
+    if (preloader.style.opacity>-0.1) {
+        preloader.style.opacity -= 0.1;
+    }
+    else
+        {
+            preloader.remove();
+        }
+}, 200);
 
 window.addEventListener('load', fadeOutEffect);
